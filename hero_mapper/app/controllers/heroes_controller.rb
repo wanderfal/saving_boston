f9b229067312711178ba5133780628fcd14e1ef2
Kane Lee <kane.m.lee@gmail.com>
2017-04-27 02:29:00 -0400

    Delete route. Move database population to seed file

--- hero_mapper/app/controllers/heroes_controller.rb
+++ hero_mapper/app/controllers/heroes_controller.rb
@@ -7,17 +7,4 @@ class HeroesController < ApplicationController
     render "heroes"
   end
 
-  def all
-    # Marvel API only returns a maximum of 100 results per call
-    # There are 1485 characters
-    # Will need to offset by 100 every call, 15 times to populate database
-    # Some results are organizations such as H.Y.D.R.A
-    # Some comics are collections of individual issues
-    api_data_total = Heroe.marvel_api_call['data']['total']
-    if api_data_total > Heroe.count
-      Heroe.save_hero_data
-    end
-  
-  end
-
 end 
