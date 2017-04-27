2d7f35621603f81a73bd8090198053c48ad33417
Kane Lee <kane.m.lee@gmail.com>
2017-04-26 23:05:08 -0400

    Move api call to Heroe model

--- hero_mapper/app/controllers/heroes_controller.rb
+++ hero_mapper/app/controllers/heroes_controller.rb
@@ -10,23 +10,22 @@ class HeroesController < ApplicationController
   def all
     # Marvel API only returns a maximum of 100 results per call
     # There are 1485 characters
+    # Will need to offset by 100 every call, 15 times to populate database
     # Some results are organizations such as H.Y.D.R.A
     # Some comics are collections of individual issues
-    url = 'https://gateway.marvel.com:443/v1/public/characters?limit=100' + '&ts=' + timestamp + '&apikey=' + ENV["MARVEL_PUBLIC"] + '&hash=' + marvel_hash
+    # url = "https://gateway.marvel.com:443/v1/public/characters?limit=100&offset=10&ts=#{timestamp}&apikey=#{ENV['MARVEL_PUBLIC']}&hash=#{marvel_hash}"
     # debugger
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    render json: JSON.parse(response)
-  end
-  
-  private
+    # uri = URI(url)
+    # response = Net::HTTP.get(uri)
+    # @response = JSON.parse(response)
+    # render json: JSON.parse(response)
 
-  def timestamp
-    Time.now.to_i.to_s
+    # test
+    # <% @response['data']['results'].each do |hero| %>
+    #  <%= hero['name'] %>
+    # <% end %>
+    # render "heroes"
+    Heroe.save_hero_data
   end
-  
-  def marvel_hash
-    hash = Digest::MD5.hexdigest( timestamp + ENV['MARVEL_PRIVATE'] + ENV['MARVEL_PUBLIC'] )
-  end
-  
+
 end 
