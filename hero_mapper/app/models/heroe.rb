73372378d709080c65b799aa8c1026de53f465e8
Kane Lee <kane.m.lee@gmail.com>
2017-04-27 00:51:15 -0400

    Update to validate on unique marvel_id. Create loop to populate database

--- hero_mapper/app/models/heroe.rb
+++ hero_mapper/app/models/heroe.rb
@@ -1,30 +1,30 @@
 class Heroe < ApplicationRecord
-  validates :name, uniqueness: true
+  validates :marvel_id, uniqueness: true
 
   def self.save_hero_data
-    # offset = 0
     response_count = nil
-    # until response_count == 0
-      # character_data = marvel_api_call
-      # if character_data['data']['results'].empty?
-
-      # end
+    until response_count == 0
+      character_data = marvel_api_call['data']
+      if !character_data['results'].empty?
+        character_data['results'].each do |hero|
+          character = Heroe.new( :name => hero['name'], :comics => hero['comics']['available'] )
+          character.save
+        end
+        response_count = character_data['count']
+      else
+        break
+      end
       
-    # end
+    end
   end
   
   private
 
   def self.marvel_api_call
-    url = "https://gateway.marvel.com:443/v1/public/characters?limit=100&offset=#{offset}&ts=#{timestamp}&apikey=#{ENV['MARVEL_PUBLIC']}&hash=#{marvel_hash}"
-    debugger
+    url = "https://gateway.marvel.com:443/v1/public/characters?orderBy=modified&limit=100&offset=#{Heroe.count}&ts=#{timestamp}&apikey=#{ENV['MARVEL_PUBLIC']}&hash=#{marvel_hash}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     response = JSON.parse(response)
-  end
-
-  def self.offset
-    Heroe.count
   end
     
   def self.marvel_hash
