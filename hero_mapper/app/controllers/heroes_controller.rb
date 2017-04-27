7d0d4912c55b7b85c441e27dc55150625d92c6d6
Kane Lee <kane.m.lee@gmail.com>
2017-04-26 23:44:22 -0400

    Create condition to only save to database when there are new entries

--- hero_mapper/app/controllers/heroes_controller.rb
+++ hero_mapper/app/controllers/heroes_controller.rb
@@ -13,19 +13,11 @@ class HeroesController < ApplicationController
     # Will need to offset by 100 every call, 15 times to populate database
     # Some results are organizations such as H.Y.D.R.A
     # Some comics are collections of individual issues
-    # url = "https://gateway.marvel.com:443/v1/public/characters?limit=100&offset=10&ts=#{timestamp}&apikey=#{ENV['MARVEL_PUBLIC']}&hash=#{marvel_hash}"
-    # debugger
-    # uri = URI(url)
-    # response = Net::HTTP.get(uri)
-    # @response = JSON.parse(response)
-    # render json: JSON.parse(response)
-
-    # test
-    # <% @response['data']['results'].each do |hero| %>
-    #  <%= hero['name'] %>
-    # <% end %>
-    # render "heroes"
-    Heroe.save_hero_data
+    api_data_total = Heroe.marvel_api_call['data']['total']
+    if api_data_total > Heroe.count
+      Heroe.save_hero_data
+    end
+  
   end
 
 end 
