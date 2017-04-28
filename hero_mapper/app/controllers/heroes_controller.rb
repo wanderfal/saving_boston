1934b016d1428a823f9571e69e57039296556ca2
Kane Lee <kane.m.lee@gmail.com>
2017-04-28 17:58:50 -0400

    Create nearby heroes

--- hero_mapper/app/controllers/heroes_controller.rb
+++ hero_mapper/app/controllers/heroes_controller.rb
@@ -4,6 +4,7 @@ class HeroesController < ApplicationController
 
   def index
     @popular_heroes = Heroe.popular_heroes
+    @nearby_heroes = Heroe.nearby_heroes('Boston')
     render "heroes"
   end
 
